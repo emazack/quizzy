364310bc750137c14887e4837aa9d553d15c6020
Emanuele Zaccaria <em.zaccaria@gmail.com>
2025-10-11 10:44:08 +0200

    [1] First graphical iteration

--- quizzy/src/App.jsx
+++ quizzy/src/App.jsx
@@ -1,18 +1,29 @@
 import './App.css'
+import blobOne from './assets/blob1.png'
+import blobTwo from './assets/blob2.png'
+
 
 function App() {
 
   return (
     <main className='main-element'>
-      <h1 className='title'>
-        Quizzy
-      </h1>
-      <p className='description'>
-        Are you Expert of Javascript?
-      </p>
-      <button className='start-button'>
-        Start quiz
-      </button>
+      <div className='content-wrapper'>
+        <h1 className='title'>
+          Quizzy
+        </h1>
+        <p className='description'>
+          Are you Expert of Javascript?
+        </p>
+        <button className='start-button'>
+          Start quiz
+        </button>
+      </div>
+      <div className='blob-container one'>
+        <img className='blob-image' src={blobOne} alt="decorative image blob color one" />
+      </div>
+      <div className='blob-container two'>
+        <img className='blob-image' src={blobTwo} alt="decorative image blob color two" />
+      </div>
     </main>
   )
 }
