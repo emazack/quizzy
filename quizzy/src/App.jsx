e8ad39e13010a779d08f1685f0bd166ac7b78b32
Emanuele Zaccaria <em.zaccaria@gmail.com>
2025-10-11 09:43:24 +0200

    [0] Start up

---
+++ quizzy/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main className='main-element'>
+      <h1 className='title'>
+        Quizzy
+      </h1>
+      <p className='description'>
+        Are you Expert of Javascript?
+      </p>
+      <button className='start-button'>
+        Start quiz
+      </button>
+    </main>
+  )
+}
+
+export default App
